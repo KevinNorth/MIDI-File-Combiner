cecd69041842acdd188828be1e8caee5e0131599
Kevin North <knorth@huskers.unl.edu>
2014-02-23 16:49:08 -0600

    Creating sequencer with copied data

--- MIDI Combiner/src/midi/combiner/MIDICombiner.java
+++ MIDI Combiner/src/midi/combiner/MIDICombiner.java
@@ -14,11 +14,31 @@ public class MIDICombiner {
     public static void main(String[] args)
             throws Exception //Since this is a proof-of-concept program, we won't worry about robust error-handling
     {
-        Sequencer mainSequencer = openSequencer();
+        Sequence file1Sequence = openMidiFile("1.mid");
+        Sequence file2Sequence = openMidiFile("2.mid");
+
+        Sequencer mainSequencer = openSequencer(file1Sequence.getDivisionType(),
+                file1Sequence.getResolution(), 2);
+
+        
+        Track file1Track = file1Sequence.getTracks()[0];
+        Track file2Track = file2Sequence.getTracks()[0];
+        
+        Track mainTrack1 = mainSequencer.getSequence().createTrack();
+        Track mainTrack2 = mainSequencer.getSequence().createTrack();
+        
+        copyEventsToTrack(file1Track, mainTrack1);
+        copyEventsToTrack(file2Track, mainTrack2);
+        
+        
     }
     
-    public static Sequencer openSequencer() throws MidiUnavailableException {
-        return MidiSystem.getSequencer();
+    public static Sequencer openSequencer(float divisionType, int resolution,
+            int numTracks) throws MidiUnavailableException, InvalidMidiDataException {
+        Sequencer sequencer = MidiSystem.getSequencer();
+        Sequence sequence = new Sequence(divisionType, resolution, numTracks);
+        sequencer.setSequence(sequence);
+        return sequencer;
     }
     
     public static Sequence openMidiFile(String fileName) throws IOException, InvalidMidiDataException {
