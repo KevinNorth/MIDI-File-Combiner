efb0be9948d0c256ea0062086c7297dc95e676a5
Kevin North <knorth@huskers.unl.edu>
2014-02-23 16:17:20 -0600

    Created NetBeans Project

---
+++ MIDI Combiner/src/midi/combiner/MIDICombiner.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midi.combiner;
+
+/**
+ *
+ * @author ThePondermatic
+ */
+public class MIDICombiner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
