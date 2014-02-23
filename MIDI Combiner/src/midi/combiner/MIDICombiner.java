03224e8ef7b0f2b6644ef91ee4f6d388e1ed5888
Kevin North <knorth@huskers.unl.edu>
2014-02-23 16:37:08 -0600

    Copy MIDI events between tracks

--- MIDI Combiner/src/midi/combiner/MIDICombiner.java
+++ MIDI Combiner/src/midi/combiner/MIDICombiner.java
@@ -5,23 +5,33 @@ import java.io.IOException;
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
+import javax.sound.midi.MidiEvent;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Track;
 
 public class MIDICombiner {
     public static void main(String[] args)
             throws Exception //Since this is a proof-of-concept program, we won't worry about robust error-handling
     {
-        Sequencer sequencer = openSequencer();
+        Sequencer mainSequencer = openSequencer();
     }
     
     public static Sequencer openSequencer() throws MidiUnavailableException {
         return MidiSystem.getSequencer();
     }
     
-    public static void openMidiFile(String fileName, Sequencer sequencer) throws IOException, InvalidMidiDataException {
+    public static Sequence openMidiFile(String fileName) throws IOException, InvalidMidiDataException {
         File file = new File(fileName);
-        Sequence seq = MidiSystem.getSequence(file);
-        sequencer.setSequence(seq);
+        return MidiSystem.getSequence(file);
+    }
+    
+    public static void copyEventsToTrack(Track sourceTrack, Track destinationTrack) {
+        int numEvents = sourceTrack.size();
+        
+        for(int i = 0; i < numEvents; i++) {
+            MidiEvent event = sourceTrack.get(i);
+            destinationTrack.add(event);
+        }
     }
 }
